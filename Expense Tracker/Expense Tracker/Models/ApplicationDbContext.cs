23b040b57cbe1ee6cdb2f47806a2bb1a280fb7e6
yasirusman85 <140712050+yasirusman85@users.noreply.github.com>
2024-08-13 15:57:19 +0500

    Add files via upload

---
+++ Expense Tracker/Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) 
+        {}
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Catagory> Catagories { get; set; }
+              
+
+    }
+}
